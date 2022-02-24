92d6f689987d6a1b852fcf6f4c0c0e4c868a789e
William Laughton <wlau390@aucklanduni.ac.nz>
2022-02-24 19:26:42 +1300

    Converted to class and simulated 100 runs

--- DCA optimise.py
+++ DCA optimise.py
@@ -1,76 +1,106 @@
 import numpy as np
 import pandas as pd
+import random
 
-btc_prices = pd.read_csv("BTC_NZD 2019-22.txt")
-fear_greed = pd.read_csv("crypto fear and greed.txt")
+class DCA():
 
-btc_prices = btc_prices.drop(labels = range(0,5),axis=0)
-fear_greed = fear_greed.drop(labels = range(0,5),axis=0)
+    def __init__(self):
 
-btc_prices.Price = [elem.replace(',', '') for elem in btc_prices.Price]
+        self.btc_prices = pd.read_csv("BTC_NZD 2019-22.txt")
+        self.fear_greed = pd.read_csv("crypto fear and greed.txt")
 
-fear_greed_values = fear_greed.values[:,1]
+        self.btc_prices = self.btc_prices.drop(labels = range(0,5),axis=0)
+        self.fear_greed = self.fear_greed.drop(labels = range(0,5),axis=0)
 
-btc_buy_prices = btc_prices.iloc[::7, :]
+        self.btc_prices.Price = [elem.replace(',', '') for elem in self.btc_prices.Price]
 
-#STEPS
+        self.fear_greed_values = self.fear_greed.values[:,1]
 
-# Go thru in steps of 7 (Wednesdays)
-# Get average value from previous week
-# 
-
-#
-
-weekly_fear_greed = np.array_split(fear_greed_values,int(len(fear_greed_values)/7))
-
-avg_weekly_fear_greed = [np.mean(elem) for elem in weekly_fear_greed]
-
-buy_dict = {}
-
-num_splits = 20
-
-for i in range(num_splits):
-    buy_dict[i] = 300 - i * 10
-
-btc_bought = 0
-total_spent = 0
-for i in range(len(btc_buy_prices)):
-
-    key = int(avg_weekly_fear_greed[i]/10)
-    total_spent = total_spent + buy_dict[key]
-    btc_bought = btc_bought + buy_dict[key] / float(btc_buy_prices.iloc[i].Price)
-
-print('DCA with Fear-greed index')
-print(f'Total spent: ${total_spent}')
-print(f'Amount spent per week: {total_spent/len(btc_buy_prices)}')
-print(f'BTC bought: {btc_bought}')
-print(f'Average BTC cost price: {total_spent/btc_bought}')
+        self.btc_buy_prices = self.btc_prices.iloc[::7, :]
 
 
-print('DCA blindly')
-btc_bought = 0
-total_spent = 0
+        self.weekly_fear_greed = np.array_split(self.fear_greed_values,int(len(self.fear_greed_values)/7))
 
-amount_spent = 200
-for i in range(len(btc_buy_prices)):
+        self.avg_weekly_fear_greed = [np.mean(elem) for elem in self.weekly_fear_greed]
+
+        self.buy_dict = {}
+
+
+    def calculate_btc(self):
+
+        btc_bought = 0
+        total_spent = 0
+        for i in range(len(self.btc_buy_prices)):
+
+            key = int(self.avg_weekly_fear_greed[i]/10)
+            total_spent = total_spent + self.buy_dict[key]
+            btc_bought = btc_bought + self.buy_dict[key] / float(self.btc_buy_prices.iloc[i].Price)
+
+        avg_price = total_spent / btc_bought
+        return avg_price
+
 
     
-    total_spent = total_spent + amount_spent
-    btc_bought = btc_bought + amount_spent / float(btc_buy_prices.iloc[i].Price)
-
-print(f'Total spent: ${total_spent}')
-print(f'Amount spent per week: {total_spent/len(btc_buy_prices)}')
-print(f'BTC bought: {btc_bought}')
-print(f'Average BTC cost price: {total_spent/btc_bought}')
 
 
+    # num_splits = 20
+
+    # for i in range(num_splits):
+    #     self.buy_dict[i] = 400 - i * 40
+
+    
+    # print('DCA with Fear-greed index')
+    # print(f'Total spent: ${total_spent}')
+    # print(f'Amount spent per week: {total_spent/len(self.btc_buy_prices)}')
+    # print(f'BTC bought: {btc_bought}')
+    # print(f'Average BTC cost price: {total_spent/btc_bought}')
+
+    def random_search(self):
+
+        num_iters = 100
+        avg_prices = []
+        coeffs_lst = []
+        for n in range(num_iters):
+
+            num_splits = 10
+            spend_coeffs = sorted(random.choices(range(400),k=num_splits),reverse=True)
+
+            for i in range(num_splits):
+                self.buy_dict[i] = spend_coeffs[i]
+
+            avg_prices.append(self.calculate_btc())
+            coeffs_lst.append(spend_coeffs)
+        
+        return avg_prices, coeffs_lst
 
 
 
 
-print(2)
+    
 
 
-#TO DO: write function to grab data from past 3/4/5 years
+if __name__ == "__main__":
 
-#Write optimisation to optimise amount bought for 10 intervals (e.g. $100 if fear_greed > 90 etc)
+    btc_dca = DCA()
+
+    avg_prices, coeffs_lst = btc_dca.random_search()
+
+
+
+
+
+
+
+
+    print(2)
+
+
+    #TO DO: write function to grab data from past 3/4/5 years
+
+    #Write optimisation to optimise amount bought for 10 intervals (e.g. $100 if fear_greed > 90 etc)
+
+
+    #TO DO:
+
+    # *Write a function to extract only wanted dates for BTC
+    # *Bayesian optimisation
